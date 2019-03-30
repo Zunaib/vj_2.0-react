ac8839deb4a11751a7530f79a32fcc7b646c5659
zunaib imtiaz <zunaib60@gmail.com>
2019-03-31 00:57:59 +0500

    Axios Corrected

--- src/components/LandingPage/ContactUsForm/ContactUsForm.js
+++ src/components/LandingPage/ContactUsForm/ContactUsForm.js
@@ -53,7 +53,7 @@ class ContactUsForm extends Component {
     loading: false
   }
 
-  
+
   fieldclearHandler = () => {
     let updatedcontactUsForm = {
       ...this.state.contactUsForm
@@ -82,15 +82,21 @@ class ContactUsForm extends Component {
       email: formData.email,
       message: formData.message
     }
+    console.log("Reached in");
+
     axios.post('/api/contactUs', contactUs)
       .then(response => {
         this.setState({ loading: false });
+        console.log("Reached in S");
         this.props.history.push('/');
       })
       .catch(error => {
         this.setState({ loading: false });
+        console.log("Reached in E");
       });
-    this.fieldclearHandler();
+
+      this.fieldclearHandler();
+      
   }
 
   checkValidity(value, rules) {
